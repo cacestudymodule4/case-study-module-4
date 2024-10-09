9dbd72773c5786438ec5aebc1338b459767bca76
pham-van-man <phamvanman.com.vn@gmail.com>
2024-10-09 16:16:25 +0700

    push man

--- case_study_module_4/src/main/java/org/example/case_study_module_4/service/FollowService.java
+++ case_study_module_4/src/main/java/org/example/case_study_module_4/service/FollowService.java
@@ -1,5 +1,6 @@
 package org.example.case_study_module_4.service;
 
+import org.example.case_study_module_4.model.Follow;
 import org.example.case_study_module_4.model.User;
 
 import java.util.List;
@@ -8,4 +9,10 @@ public interface FollowService {
     List<User> findFolloweeByFollower(Long followerId);
 
     List<User> findFollowerByFollowee(Long followeeId);
+
+    void CreateFollower(Follow follow);
+
+    Follow getFollow(User follower, User followee);
+
+    void deleteFollow(Follow follow);
 }
--- case_study_module_4/src/main/java/org/example/case_study_module_4/service/FriendshipServiceImpl.java
+++ case_study_module_4/src/main/java/org/example/case_study_module_4/service/FriendshipServiceImpl.java
@@ -1,5 +1,6 @@
 package org.example.case_study_module_4.service;
 
+import org.example.case_study_module_4.model.Friendship;
 import org.example.case_study_module_4.model.User;
 import org.example.case_study_module_4.repository.FriendshipRepository;
 import org.springframework.stereotype.Service;
@@ -22,5 +23,37 @@ public class FriendshipServiceImpl implements FriendshipService {
         friends.addAll(friendshipRepository.findFriendsByUserId(user.getId(), status));
         friends.addAll(friendshipRepository.findFriendsByFriendId(user.getId(), status));
         return friends;
+    }
+
+    @Override
+    public String getStatus(User user, User otherUser) {
+        Friendship friendship = friendshipRepository.findFriendshipByUserIdAndFriendId(user.getId(), otherUser.getId());
+        if (friendship != null) {
+            return friendship.getStatus();
+        }
+        return "notFriend";
+    }
+
+    @Override
+    public void deleteFriend(Friendship friendship) {
+        friendshipRepository.delete(friendship);
+    }
+
+    @Override
+    public Friendship getFriendship(User user, User otherUser) {
+        return friendshipRepository.findFriendshipByUserIdAndFriendId(user.getId(), otherUser.getId());
+    }
+
+    @Override
+    public Friendship createFriendship(User user, User otherUser) {
+        Friendship friendship = new Friendship();
+        friendship.setUser(user);
+        friendship.setFriend(otherUser);
+        return friendshipRepository.save(friendship);
+    }
+
+    @Override
+    public void updateFriendship(Friendship friendship) {
+        friendshipRepository.save(friendship);
     }
 }
